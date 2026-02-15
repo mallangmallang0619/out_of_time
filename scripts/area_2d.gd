409977a4829c21e106b2e5a841af84ee317e0174
JReeseSanchez <jaiden.sanchez@ucalgary.ca>
2026-02-14 20:01:46 -0700

    Started Random Item Spawn

---
+++ scripts/area_2d.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var point1: Vector2 = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

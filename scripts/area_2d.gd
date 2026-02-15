e2143e33e0e2d896055b51e6584c691acec386b7
JReeseSanchez <jaiden.sanchez@ucalgary.ca>
2026-02-14 21:03:24 -0700

    Update player movement

--- scripts/area_2d.gd
+++ scripts/area_2d.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
 @export var point1: Vector2 = Vector2(0,0)
-@export var point2: Vector2 = Vector2()
+@export var point2: Vector2 = Vector2(400,300)
+
+func get_random_point(p1: Vector2, p2: Vector2) -> Vector2:
+	
 
 
 # Called when the node enters the scene tree for the first time.

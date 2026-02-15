c1ac69f6920b64ced9bcef376ac65ab8b9758b78
mallangmallang0619 <115538956+mallangmallang0619@users.noreply.github.com>
2026-02-14 19:45:39 -0700

    movement changes
    movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,54 @@
 extends CharacterBody2D
 
+@export var speed = 100
+@export var jump_speed = -200
+@export var acceleration = 10
+@export var friction = 6
+@onready var _animated_sprite = $AnimatedSprite2D
+var jump_counter = 0
 
-const SPEED = 200
-const JUMP_VELOCITY = -350.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var controls = {"move_right": [KEY_RIGHT, KEY_D],
+"move_left": [KEY_LEFT, KEY_A],
+"jump": [KEY_UP, KEY_W, KEY_SPACE]}
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _ready():
+	add_inputs()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func add_inputs():
+	var ev
+	for action in controls:
+		if not InputMap.has_action(action):
+			InputMap.add_action(action)
+		for key in controls[action]:
+			ev = InputEventKey.new()
+			ev.keycode = key
+			InputMap.action_add_event(action, ev)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+#delta is per frame
+#basic input handler
+func get_input(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	if(is_on_floor()):
+		jump_counter = 0
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = lerp(velocity.x, direction  *speed, acceleration*  delta)
+		#_animated_sprite.play("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = lerp(velocity.x, 0.0, friction * delta)
+		#_animated_sprite.play("Idle")
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+	elif Input.is_action_just_pressed("jump") and not is_on_floor() and jump_counter == 0:
+		velocity.y = 0
+		velocity.y = jump_speed
+		jump_counter += 1
+	
 
+
+func _physics_process(delta):
+
+	velocity.y += gravity*  delta
+	get_input(delta)
 	move_and_slide()
